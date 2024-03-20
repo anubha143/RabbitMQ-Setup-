d807790e141d4ef9491352509d34185fa18f409d
Anubha Pant <pant.anubha24@gmail.com>
2024-03-20 15:39:38 +0530

    added msg logic for product canceled, delivered, & in-transit

--- src/server.js
+++ src/server.js
@@ -7,36 +7,47 @@ const main = async () => {
     "amqps://ybbpakop:rs0knoxchoJz6XGkqCMumuJpSlb9oBZp@puffin.rmq2.cloudamqp.com/ybbpakop"
   );
   const channel = await connection.createChannel();
-  await mongo();
+  const db = await mongo();
+
   channel.assertQueue("notification");
   channel.consume(
     "notification",
     async (msg) => {
       console.log(msg.content.toString());
       const parseMsg = JSON.parse(msg.content.toString());
+
+      const collection = db.collection("orders");
+      const productCollection = db.collection("products");
+
+      const order = await collection.findOne({
+        _id: new ObjectId(parseMsg.data.id),
+      });
+      const product = await productCollection.findOne({
+        _id: new ObjectId(order.productId),
+      });
+
+      let smsMsg;
+
       console.log("parseMsg", parseMsg);
       switch (parseMsg.type) {
         case "order-placed":
-          const db = await mongo();
-          const collection = db.collection("orders");
-          const order = await collection.findOne({
-            _id: new ObjectId(parseMsg.data.id),
-          });
-          const productCollection = db.collection("products");
-          const product = await productCollection.findOne({
-            _id: new ObjectId(order.productId),
-          });
-          const smsMsg = `Hi ${order.customer_name}, Order placed for ${product.name} with price Rs. ${order.total_cost}`;
+          smsMsg = `Hi ${order.customer_name}, Order placed for ${product.name} with price Rs.${order.total_cost}`;
           console.log(smsMsg);
           break;
 
         case "order-cancelled":
+          smsMsg = `Hi ${order.customer_name}, Order placed for ${product.name} with price Rs.${order.total_cost} has been cancelled`;
+          console.log(smsMsg);
           break;
 
         case "order-in-transit":
+          smsMsg = `Hi ${order.customer_name}, Order placed for ${product.name} with price Rs.${order.total_cost} has been shipped`;
+          console.log(smsMsg);
           break;
 
         case "order-delivered":
+          smsMsg = `Hi ${order.customer_name}, Order placed for ${product.name} with price Rs.${order.total_cost} has been delivered`;
+          console.log(smsMsg);
           break;
 
         default:
